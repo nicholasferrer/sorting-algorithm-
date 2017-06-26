cea455ca09b782d7591655e4a5d6a5030ca16b1c
Nicholas Ferrer <nicholasferrer@hotmail.com>
2017-06-26 19:13:48 -0300

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -13,17 +13,18 @@ def cria_vetor(num):
 def InsertionSort(num):
     lista = cria_vetor(num)
     print(lista)
-    for i in range(1,num):
-        if lista[i] < lista[i-1]:
-            aux = lista[i-1]
-            lista[i-1] = lista[i]
-            lista[i] = aux
-        while (i-1) != 0:
-            i = i-1
-            if lista[i] < lista[i - 1]:
-                aux = lista[i - 1]
-                lista[i - 1] = lista[i]
-                lista[i] = aux
+    for i in range(1, len(lista)):
+        teste = True
+        pos = i
+        for j in range(i):  # Acha a posição do primeiro elemento menor que o elemento atual
+            if lista[i] < lista[j] and teste:
+                pos = j
+                teste = False
+        aux = lista[i]  # Salva o valor atual
+        for j in range(i, pos, -1):  # Passa todos os elementos entre 'pos' e 'i' uma casa pra frente
+            lista[j] = lista[j - 1]
+        lista[pos] = aux                # Bota o valor atual na posição certa
+
     return lista
 
 num = int(input("Quantos numeros deseja no vetor?" ))
